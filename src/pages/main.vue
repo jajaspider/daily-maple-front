efbba03df9e05c430b243c530895b4cf69be6ba7
ChoiChangYong <34606508+ChoiChangYong@users.noreply.github.com>
2021-02-23 00:10:49 +0900

    feat(#5): Modify main page template
    로고에 색상을 넣기 위해 메인 페이지 템플릿을 헤더가 흰색인 템플릿으로 변경합니다.

--- src/pages/main.vue
+++ src/pages/main.vue
@@ -1,79 +1,64 @@
 <template>
   <div id="main_div">
     <!-- Home Section start-->
-
-    <!--Home Section start-->
-    <section id="home" class="home home-two vertical-scrolling">
+    <section id="home" class="home">
+      <div class="home-decor">
+        <div class="home-circle1">
+          <img src="@/assets/images/main-banner3.png" alt="" />
+        </div>
+        <div class="home-circle2">
+          <img src="@/assets/images/main-banner12.png" alt="" />
+        </div>
+        <div class="home-circle3">
+          <img src="@/assets/images/main-banner1.png" alt="" />
+        </div>
+      </div>
       <div class="container">
         <div class="row">
-          <div class="col-md-5 col-sm-12">
+          <div class="col-md-5">
             <div class="home-contain">
-              <div class="text-white">
-                <div class="contain">
-                  <img src="@/assets/images/home2/logo-icon.png" alt="logo" />
-                  <h4>Landing page for</h4>
-                  <h1 class="text-white">
-                    t<span class="f-bold ">o</span>v<span class="f-bold "
-                      >o</span
-                    >
-                    <span class="f-bold f-color text-white">app</span>
-                  </h1>
-                  <p class="slide-cap-desc">
-                    Enhance your business ideas with Powerful, Responsive,
-                    Elegant TOVO Theme.
-                  </p>
-                  <a href="#"
-                    ><img
-                      src="@/assets/images/appstore.png"
-                      alt="appstore"
-                      class="store"
-                  /></a>
-                  <a href="#"
-                    ><img
-                      class="ml-10 store"
-                      src="@/assets/images/play-store.png"
-                      alt="play-store"
-                  /></a>
-                </div>
-                <div class="play-button">
-                  <a
-                    v-on:click="isHidden = !isHidden"
-                    class="popup-vimeo animated-circle"
-                    ><img
-                      src="@/assets/images/home2/play-button.png"
-                      alt="play-button"
-                      class="img-fluid"
-                  /></a>
-                </div>
-                <div
-                  v-on:click="isHidden = !isHidden"
-                  v-if="!isHidden"
-                  :player-height="100"
-                  class="white-popup"
-                >
-                  <vimeo-player
-                    :player-width="940"
-                    :player-height="529"
-                    class="vimeo-player"
-                    ref="player"
-                    :video-id="videoid"
+              <div>
+                <img
+                  src="@/assets/images/logoimage.png"
+                  alt="caption-img"
+                  class="caption-img"
+                />
+                <h4>Landing page for</h4>
+                <h1>
+                  t<span class="f-bold">o</span>v<span class="f-bold">o</span>
+                  <span class="f-bold f-color">app</span>
+                </h1>
+                <p>
+                  Enhance your business ideas with Powerful, Responsive, Elegant
+                  TOVO Theme.
+                </p>
+                <a href="#">
+                  <img
+                    src="@/assets/images/appstore.png"
+                    alt="appstore"
+                    class="store"
                   />
-                </div>
+                </a>
+                <a href="#">
+                  <img
+                    class="ml-10 store"
+                    src="@/assets/images/play-store.png"
+                    alt="play-store"
+                  />
+                </a>
               </div>
             </div>
           </div>
-          <div class="col-sm-7">
+          <div class="col-sm-6 offset-md-1">
             <div class="home-right">
               <!-- Tile js start -->
-              <Tile :image="'home2/slider-caption.png'" />
+              <Tile :image="'mobile.png'" />
               <!-- Tile js end -->
             </div>
           </div>
         </div>
       </div>
     </section>
-    <!--Home Section End-->
-
     <!--Home Section End -->
 
     <!-- About Start  -->
@@ -126,14 +111,12 @@
 <script>
 export default {
   name: 'Main',
-  data() {
-    return {
-      isHidden: true,
-      videoid: 54298665,
-    };
-  },
   beforeCreate: function() {
-    document.body.className = 'home-style-two';
+    document.body.className = '';
   },
 };
 </script>
+<!--remove comment below line to set color -->
+<!--<style lang="css">-->
+<!--@import '../public/assets/css/color/color-2.css';-->
+<!--</style>-->
